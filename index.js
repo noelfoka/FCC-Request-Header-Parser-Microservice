3f02f51ab915415d1c72420d1f3cf1eee23a51a6
noelfoka <116439136+noelfoka@users.noreply.github.com>
2023-05-27 07:56:50 +0000

    second project completed

--- index.js
+++ index.js
@@ -20,8 +20,15 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
 });
 
 // listen for requests :)
